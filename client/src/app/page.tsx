7664f3cbf73b1db5fc865e691af9c46c1fef2b31
Xavier Fabrega Pascual <xavifabrega2@hotmail.com>
2024-01-12 15:41:16 +0100

    userslist comp not found

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,6 +1,6 @@
 import styles from "./page.module.css";
-import UsersList from "@/components/UsersList/UsersList";
 import SynchronizeBtn from "@/components/SynchronizeBtn/SynchronizeBtn";
+import UsersList from "@/components/UsersList/UsersList";
 
 export default function Home() {
   return (
