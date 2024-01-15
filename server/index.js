7c28c52e3b3772fc1e8860b896938db71cf541a4
Xavier Fabrega Pascual <xavifabrega2@hotmail.com>
2024-01-15 17:00:42 +0100

    vercel config

--- server/index.js
+++ server/index.js
@@ -10,7 +10,14 @@ const userRoutes = require("./src/routes/userRoutes");
 const port = process.env.PORT || 4000;
 app.use(express.json());
 app.use(cors());
+// app.use(cors({
+//   origin: ["url"],
+//   methods: ["POST", "GET"]
+// }));
 app.use(userRoutes);
+app.get("/", (req, res) => {
+  res.json("hello");
+});
 
 connectToDB()
   .then(() => {
